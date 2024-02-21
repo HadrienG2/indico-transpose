9da3288e00c997964a8486918a1b4818c26f3f68
Hadrien Grasland <hadrien.grasland@ijclab.in2p3.fr>
2024-02-21 16:03:00 +0100

    Fixes brought by discussion with David
    - Allow fields which we don't care about yet to be absent
    - Sort people by increasing registration date
    - Print output as an ordered list, not an unordered list
    - Replace messy affiliations with shorter ones
    
    With this, the main function started to get a little crowded, so I rewrote the
    program in a more modular fashion.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,20 @@
 use clap::Parser;
+use log::{debug, warn};
 use regex::Regex;
 use serde::Deserialize;
 use std::{
     collections::{BTreeMap, HashMap},
-    rc::Rc,
+    fmt::Display,
+    fs::File,
+    sync::{Arc, OnceLock},
 };
+use time::{Date, Month, OffsetDateTime, Time};
 
-/// Information about people that we certainly care about right now
-#[allow(unused)]
-#[derive(Debug, Deserialize)]
-struct Identity {
-    #[serde(rename = "Name")]
-    name: String,
-    #[serde(rename = "Email Address")]
-    email_address: String,
-    #[serde(rename = "Affiliation")]
-    affiliation: String,
-}
-
-/// Full indico record featuring a bit of extra info we may care about someday
-#[allow(unused)]
-#[derive(Debug, Deserialize)]
-struct Record {
-    #[serde(rename = "ID")]
-    id: usize,
-    #[serde(flatten)]
-    identity: Identity,
-    #[serde(rename = "Choice of modules")]
-    choice_of_modules: String,
-    #[serde(rename = "Registration state")]
-    registration_state: String,
-    #[serde(rename = "Checked in")]
-    checked_in: String,
-}
+// === CLI starts here ===
 
 /// Translate Indico's per-user registrations into per-course registrations
 #[derive(Parser)]
 struct Args {
-    /// Log raw user registration records for debugging
-    #[arg(long, default_value_t = false)]
-    debug: bool,
-
     /// Path to Indico CSV
     #[arg(default_value_t = String::from("registrations.csv".to_owned()))]
     input_path: String,
@@ -49,72 +23,242 @@ struct Args {
 fn main() -> csv::Result<()> {
     // Set up app
     let args = Args::parse();
-    let mut csv_reader = csv::Reader::from_path(args.input_path)?;
-    let mut registrations_by_module = HashMap::<_, Vec<_>>::new();
+    env_logger::init();
 
-    // Parse registration records, group identities by module
-    if args.debug {
-        eprintln!("DEBUG: Parsing registration records...\n");
-    }
-    for result in csv_reader.deserialize() {
-        if args.debug {
-            eprintln!("{result:#?}");
-        }
-        let record: Record = result?;
-        let identity = Rc::new(record.identity);
-        for module in record.choice_of_modules.split(';') {
-            registrations_by_module
-                .entry(module.trim().to_owned())
+    // Read out raw CSV records
+    let csv_reader = csv::Reader::from_path(args.input_path)?;
+    let raw_records = load_raw_records(csv_reader)?;
+
+    // Translate records into a more exploitable data layout
+    let registrations = Registrations::new(raw_records);
+
+    // Order people by registration time
+    let persons_by_registration_time = registrations
+        .persons
+        .iter()
+        .enumerate()
+        .map(|(person_id, person)| (person.registration_time, person_id))
+        .collect::<BTreeMap<OffsetDateTime, PersonId>>();
+
+    // For each module, produce a matching ordered list of who registered
+    let mut module_to_ordered_persons =
+        HashMap::<ModuleId, Vec<PersonId>>::with_capacity(registrations.modules.len());
+    for (_, person_id) in persons_by_registration_time {
+        for &module_id in &registrations.persons[person_id].choice_of_modules {
+            module_to_ordered_persons
+                .entry(module_id)
                 .or_default()
-                .push(identity.clone());
+                .push(person_id);
         }
     }
-    if args.debug {
-        eprintln!();
-    }
 
-    // Sort modules by starting date, if available
-    if args.debug {
-        eprintln!("DEBUG: Sorting modules by start time...");
-    }
-    let date_regex = Regex::new(
-        r"([0-9]{1,2})/([0-9]{1,2})(?: \+ [a-z]+. [0-9]+/[0-9]+)?, ([0-9]{1,2})[:h]([0-9]{1,2})",
-    )
-    .unwrap();
-    let mut modules_by_date = BTreeMap::new();
-    for (idx, module_name) in registrations_by_module.keys().enumerate() {
-        let timestamp = if let Some((_, day_month_hour_min)) =
-            date_regex.captures(module_name).map(|cap| cap.extract())
-        {
-            let [day, month, hour, min] = day_month_hour_min.map(|s| s.parse::<usize>().unwrap());
-            let mut result = month;
-            result = result * 31 + day;
-            result = result * 24 + hour;
-            result = result * 60 + min;
-            result
-        } else {
-            if args.debug {
-                eprintln!("WARNING: Couldn't parse start time of module {module_name}, it will be unordered in output");
-            }
-            usize::MAX - idx
-        };
-        modules_by_date.insert(timestamp, module_name);
-    }
-    if args.debug {
-        eprintln!();
-    }
+    // Order modules by start time
+    let modules_by_start_time = registrations
+        .modules
+        .iter()
+        .enumerate()
+        .map(|(module_id, module)| (module.start_time, module_id))
+        .collect::<BTreeMap<OffsetDateTime, ModuleId>>();
 
-    // Display final per-module registration
+    // Display module registrations
     println!("# Registrations to each module");
-    for (_, module) in modules_by_date {
-        println!("\n## {module}\n");
-        for registration in &registrations_by_module[module] {
-            print!("- `{} <{}>`", registration.name, registration.email_address);
-            if !registration.affiliation.is_empty() {
-                print!(" from {}", registration.affiliation);
-            }
-            println!();
+    for (_, module_id) in modules_by_start_time {
+        println!("\n## {}\n", registrations.modules[module_id].name);
+        for (idx, &person_id) in module_to_ordered_persons[&module_id].iter().enumerate() {
+            println!("{}. {}", idx + 1, registrations.persons[person_id].identity);
         }
     }
     Ok(())
+}
+
+// === Input data from Indico ===
+
+/// Full indico record featuring a bit of extra info we're not using yet
+#[allow(unused)]
+#[derive(Debug, Deserialize)]
+struct CSVRow {
+    #[serde(rename = "ID")]
+    id: Option<usize>,
+    #[serde(flatten)]
+    identity: Identity,
+    #[serde(rename = "Choice of modules")]
+    choice_of_modules: Box<str>,
+    #[serde(rename = "Registration date", with = "indico_datetime")]
+    registration_time: OffsetDateTime,
+    #[serde(rename = "Registration state")]
+    registration_state: Option<Box<str>>,
+}
+
+/// Basic information about people that we want to display in the end
+#[allow(unused)]
+#[derive(Debug, Deserialize)]
+struct Identity {
+    #[serde(rename = "Name")]
+    name: Box<str>,
+    #[serde(rename = "Email Address")]
+    email_address: Box<str>,
+    #[serde(rename = "Affiliation")]
+    affiliation: Box<str>,
+}
+//
+impl Display for Identity {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "`{} <{}>`", self.name, self.email_address)?;
+        if !self.affiliation.is_empty() {
+            static SIMPLIFIED_AFFILIATIONS: OnceLock<HashMap<Box<str>, Arc<str>>> = OnceLock::new();
+            let simplified_affiliations = SIMPLIFIED_AFFILIATIONS.get_or_init(|| {
+                let mut result = HashMap::new();
+                let ijclab = Arc::<str>::from("IJCLab");
+                result.insert("Laboratoire de Physique des 2 infinis Irène Joliot-Curie, Université Paris-Saclay, CNRS-IN2P3. Université Paris-Saclay, CNRS-IN2P3".into(), ijclab.clone());
+                result.insert("IJCLAB - IN2P3 - CNRS".into(), ijclab.clone());
+                result.insert("IJCLab - IN2P3 - CNRS".into(), ijclab.clone());
+                result
+            });
+            let affiliation =
+                if let Some(simplified) = simplified_affiliations.get(&*self.affiliation) {
+                    simplified
+                } else {
+                    &*self.affiliation
+                };
+            write!(f, " from {affiliation}")?;
+        }
+        Ok(())
+    }
+}
+
+// Date/time format used by Indico
+time::serde::format_description!(
+    indico_datetime,
+    OffsetDateTime,
+    "[year]-[month]-[day] [hour]:[minute]:[second].[subsecond][offset_hour sign:mandatory]:[offset_minute]"
+);
+
+/// Load CSV records
+fn load_raw_records(mut csv_reader: csv::Reader<File>) -> csv::Result<Vec<CSVRow>> {
+    debug!("Loading CSV registration records...");
+    let mut result = Vec::new();
+    for record in csv_reader.deserialize() {
+        debug!("- {record:#?}");
+        let record: CSVRow = record?;
+        result.push(record);
+    }
+    Ok(result)
+}
+
+// === Pre-digested data ===
+
+/// Exploitable version of the Indico registration records
+#[derive(Debug, Default)]
+struct Registrations {
+    /// List of people who registered to courses
+    persons: Vec<Person>,
+
+    /// List of pedagogical modules
+    modules: Vec<Module>,
+}
+
+/// What we need to know about someone who registered to modules
+#[derive(Debug)]
+struct Person {
+    /// Information identifying this person
+    identity: Identity,
+
+    /// Which modules they chose to attend
+    choice_of_modules: Vec<ModuleId>,
+
+    /// Time at which they registered
+    registration_time: OffsetDateTime,
+}
+
+/// Index of a module within Registrations::modules
+type ModuleId = usize;
+
+/// What we know about a module
+#[derive(Debug)]
+struct Module {
+    /// Name of the module
+    name: Box<str>,
+
+    /// Date and time at which the module will start
+    start_time: OffsetDateTime,
+}
+//
+impl Module {
+    /// Create a new module entry from the module name in Indico CSV
+    fn new(module_name: &str) -> Self {
+        debug!("- Registering new module {module_name}");
+        static START_TIME_REGEX: OnceLock<Regex> = OnceLock::new();
+        let start_time_regex = START_TIME_REGEX.get_or_init(|| {
+            Regex::new(
+                r"([0-9]{1,2})/([0-9]{1,2})(?: \+ [a-z]+. [0-9]+/[0-9]+)?, ([0-9]{1,2})[:h]([0-9]{1,2})",
+            )
+            .expect("Regex was manually checked")
+        });
+        let start_time = if let Some((_, day_month_hour_min)) = start_time_regex
+            .captures(module_name)
+            .map(|cap| cap.extract())
+        {
+            let [day, month, hour, min] = day_month_hour_min.map(|s| s.parse::<usize>().unwrap());
+            OffsetDateTime::new_utc(
+                Date::from_calendar_date(2024, Month::January.nth_next(month as u8 - 1), day as u8)
+                    .expect("Module date should be valid"),
+                Time::from_hms(hour as u8, min as u8, 0).expect("Module time should be valid"),
+            )
+        } else {
+            warn!(
+                "Couldn't parse start time of module \"{module_name}\", it will be unordered in output"
+            );
+            OffsetDateTime::new_utc(Date::MAX, Time::MIDNIGHT)
+        };
+        Self {
+            name: module_name.into(),
+            start_time,
+        }
+    }
+}
+
+/// Index of a module within Registrations::modules
+type PersonId = usize;
+
+impl Registrations {
+    /// Translate raw Indico records into a more exploitable form
+    fn new(raw_records: Vec<CSVRow>) -> Self {
+        debug!("Post-processing registration records...");
+        let mut result = Self::default();
+        let mut module_to_id = HashMap::new();
+        for CSVRow {
+            identity,
+            choice_of_modules,
+            registration_time,
+            ..
+        } in raw_records
+        {
+            let module_ids = choice_of_modules
+                .split(';')
+                .map(|module_name| {
+                    let module_name = module_name.trim();
+                    // Have we seen this module before?
+                    let module_id = if let Some(module_id) = module_to_id.get(module_name) {
+                        // Reuse previous module ID
+                        *module_id
+                    } else {
+                        // Post-process module name, deduce module ID
+                        let module = Module::new(module_name);
+                        let module_name = module.name.clone();
+                        let module_id = result.modules.len();
+                        result.modules.push(module);
+                        module_to_id.insert(module_name, module_id);
+                        module_id
+                    };
+                    module_id
+                })
+                .collect();
+            result.persons.push(Person {
+                identity,
+                choice_of_modules: module_ids,
+                registration_time,
+            })
+        }
+        result
+    }
 }
